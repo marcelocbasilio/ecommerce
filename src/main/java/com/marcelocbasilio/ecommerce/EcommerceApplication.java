44d039a43a9a3ae73c9a1d08a134667d37f0ab64
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2024-10-05 19:58:28 -0300

    After deleting the dependencies.txt, ecommerce.txt and token_JWT.txt files, I modified the SecurityConfig class by adding a method to encrypt the password, to test I implemented the run method of the CommandLineRunner interface and finally I edited the import.sql file by changing the password 123456 to its encrypted value.
    Signed-off-by: Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>

--- src/main/java/com/marcelocbasilio/ecommerce/EcommerceApplication.java
+++ src/main/java/com/marcelocbasilio/ecommerce/EcommerceApplication.java
@@ -1,13 +1,26 @@
 package com.marcelocbasilio.ecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("[ENCODE] -> " + passwordEncoder.encode("123456"));
+		Boolean result = passwordEncoder.matches("123456", "$2a$10$61dTy/AUSRl4F4FBUad2l.ud8TIPI9ySHJ.8Ks2023k5kS8kLfgTO");
+		System.out.println("[CHECK PASSWORD] -> " + result);
 	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$61dTy/AUSRl4F4FBUad2l.ud8TIPI9ySHJ.8Ks2023k5kS8kLfgTO');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$61dTy/AUSRl4F4FBUad2l.ud8TIPI9ySHJ.8Ks2023k5kS8kLfgTO');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
