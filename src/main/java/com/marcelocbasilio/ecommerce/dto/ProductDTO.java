3534473023ae087a64d09e412a961b1475fbbaca
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2025-01-10 22:31:33 -0300

    Fix validation.

--- src/main/java/com/marcelocbasilio/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/marcelocbasilio/ecommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.marcelocbasilio.ecommerce.dto;
 
 import com.marcelocbasilio.ecommerce.entities.Category;
 import com.marcelocbasilio.ecommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +19,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private final String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private final Double price;
     private final String imgUrl;
